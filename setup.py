fd43381dfba7593ec982a98124e8507a9fc1dc47
Pranjal Panigrahi <panigrahipranjal32@gmail.com>
2025-12-16 17:51:12 +0530

    setup.py and requirements configured

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> List[str]:
+    """
+    This function will return the list of requirements
+    mentioned in requirements.txt file.
+    """
+    requirement_list: List[str] = []
+
+    try:
+        with open("requirements.txt", "r") as file:
+            lines = file.readlines()
+
+            for line in lines:
+                requirement = line.strip()
+
+                # ignore empty lines and -e .
+                if requirement and requirement != "-e .":
+                    requirement_list.append(requirement)
+
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_list
+
 
 setup(
     name="network_security",
@@ -6,4 +32,5 @@ setup(
     author="Pranjal Panigrahi",
     author_email="panigrahipranjal32@gmail.com",
     packages=find_packages(),
+    install_requires=get_requirements(),
 )
