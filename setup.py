b3eee242e5be2c9eb70916320e2eb7a7fbe13b2c
Pranjal Panigrahi <panigrahipranjal32@gmail.com>
2025-12-16 17:41:07 +0530

    Project structure setup is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="network_security",
+    version="0.0.1",
+    author="Pranjal Panigrahi",
+    author_email="panigrahipranjal32@gmail.com",
+    packages=find_packages(),
+)
